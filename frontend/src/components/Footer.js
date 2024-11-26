b0425ba8bfcadf35445663b338da52b0b9fef127
Anjelica <144828225+anjelica0224@users.noreply.github.com>
2024-11-27 04:10:57 +0530

    chore: rm certain files

--- frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <footer className="bg-dark text-light text-center py-3">
-    &copy; 2024 Gurulink. All rights reserved.
-  </footer>
-);
-
-export default Footer;
